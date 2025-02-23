0f6c66a93961a9eb211a9882d764f7867eddbd3d
proton1nk <23520675@gm.uit.edu.vn>
2025-02-24 01:28:10 +0700

    Chuan hoa message exception gui ve bang thong tin lay ve tu annotation

--- src/main/java/com/example/first_pj/exception/ErrorCode.java
+++ src/main/java/com/example/first_pj/exception/ErrorCode.java
@@ -10,12 +10,12 @@ public enum ErrorCode
     UNCATEGORIZED_EXCEPTION(9999,"Uncategorized Exception", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_KEY(1001,"INVALID_KEY",HttpStatus.BAD_REQUEST),
     USER_EXISITED(1002,"User existed",HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003,"USER INVALID",HttpStatus.BAD_REQUEST),
-    INVALID_PASSWORD(1004,"INVALID PASSWORD",HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003,"User name must be as least{min}",HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1004,"User name must be as least{min}",HttpStatus.BAD_REQUEST),
     USER_NOTEXISITED(1005," User not existed",HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006,"Unathenticated",HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1007,"You dont have permission ",HttpStatus.FORBIDDEN),
-    INVALID_DOB (1008,"You dont elder enough",HttpStatus.UNAUTHORIZED)
+    INVALID_DOB (1008,"Your age must be as least{min}",HttpStatus.UNAUTHORIZED)
     ;
 
     ErrorCode(int code, String message,HttpStatusCode httpStatusCode) {
--- src/main/java/com/example/first_pj/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/first_pj/exception/GlobalExceptionHandler.java
@@ -2,17 +2,22 @@ package com.example.first_pj.exception;
 
 import com.example.first_pj.dto.ApiResponse;
 
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.security.access.AccessDeniedException;
 
-
+import java.awt.*;
+import java.util.Map;
+import java.util.Objects;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private  static  final String MIN_ATTRIBUTES = "min";
 
     @ExceptionHandler(value=Exception.class)
     ResponseEntity<ApiResponse> handleRuntimeException(RuntimeException e) {
@@ -51,14 +56,26 @@ public class GlobalExceptionHandler {
     ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String enumkey  = e.getBindingResult().getFieldError().getDefaultMessage();
             ErrorCode errorcode = ErrorCode.INVALID_KEY;
+            Map attributes= null;
         try {
             errorcode = ErrorCode.valueOf(enumkey);
+            var constraintViolation = e.getBindingResult()
+                    .getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+           attributes =  constraintViolation.getConstraintDescriptor().getAttributes();
+
         }
         catch (IllegalArgumentException e1) {}
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorcode.getCode());
-        apiResponse.setMessage(errorcode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes)?mapAttributes(errorcode.getMessage(),attributes): errorcode.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+    private String mapAttributes (String message, Map<String,Object> attributes)
+    {
+        String minValue= String.valueOf(attributes.get(MIN_ATTRIBUTES));
+
+        return message.replace("{"+MIN_ATTRIBUTES+"}",minValue);
     }
 }
